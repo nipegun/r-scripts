3987f42236ebc0e04e1385c0092c9048061091f4
NiPeGun <nipegun@gmail.com>
2020-06-04 09:19:06 +0200

    Update ServidorDNS-VerLogEnTiempoReal.sh

--- ServidorDNS-VerLogEnTiempoReal.sh
+++ ServidorDNS-VerLogEnTiempoReal.sh
@@ -9,5 +9,5 @@
 #  Script de NiPeGun para ver los logs de dhcpd en tiempo real
 #---------------------------------------------------------------
 
-tail -f /var/log/syslog | grep dns
+tail -f /var/log/syslog | grep bind
 
