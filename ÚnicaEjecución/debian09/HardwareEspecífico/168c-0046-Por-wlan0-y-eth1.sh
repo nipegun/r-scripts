3b9de88bb8e6ff40433bc96ea5c7a149abf7df9a
NiPeGun <nipegun@gmail.com>
2020-09-05 15:42:04 +0200

    Update 168c-0046-Por-wlan0-y-eth1.sh

--- ÚnicaEjecución/debian09/HardwareEspecífico/168c-0046-Por-wlan0-y-eth1.sh
+++ ÚnicaEjecución/debian09/HardwareEspecífico/168c-0046-Por-wlan0-y-eth1.sh
@@ -153,27 +153,32 @@ do
       echo "-----------------------------------"
       echo ""
       echo "#/etc/hostapd/hostapd.conf" > /etc/hostapd/hostapd.conf
+      echo "" >> /etc/hostapd/hostapd.conf
+      echo "# Primer punto de acceso" >> /etc/hostapd/hostapd.conf
       echo "interface=$interfazinalambrica1" >> /etc/hostapd/hostapd.conf
-      echo "driver=nl80211" >> /etc/hostapd/hostapd.conf
-      echo "bridge=br0" >> /etc/hostapd/hostapd.conf
-      echo "hw_mode=a" >> /etc/hostapd/hostapd.conf
-      echo "wme_enabled=1" >> /etc/hostapd/hostapd.conf
-      echo "ieee80211d=1" >> /etc/hostapd/hostapd.conf
-      echo "channel=0" >> /etc/hostapd/hostapd.conf
-      echo "country_code=ES" >> /etc/hostapd/hostapd.conf
-      echo "wmm_enabled=1" >> /etc/hostapd/hostapd.conf
-      echo "ieee80211n=1" >> /etc/hostapd/hostapd.conf
-      echo "ht_capab=[RXLDPC][HT20+][HT40+][SHORT-GI-20][SHORT-GI-40][TX-STBC][RX-STBC1][MAX-AMSDU-7935][DSSS_CCK-40]" >> /etc/hostapd/hostapd.conf
-      echo "ieee80211ac=1" >> /etc/hostapd/hostapd.conf
-      echo "vht_capab=[MAX-MPDU-11454][VHT160-80PLUS80][RXLDPC][SHORT-GI-80][SHORT-GI-160][TX-STBC][SU-BEAMFORMER][SU-BEAMFORMEE][MU-BEAMFORMER][MU-BEAMFORMEE][RX-ANTENNA-PATTERN][TX-ANTENNA-PATTERN]" >> /etc/hostapd/hostapd.conf
-      echo "ignore_broadcast_ssid=0" >> /etc/hostapd/hostapd.conf
-      echo "ssid=RouterX86" >> /etc/hostapd/hostapd.conf
-      echo "eap_reauth_period=360000000" >> /etc/hostapd/hostapd.conf
       echo "wpa=2" >> /etc/hostapd/hostapd.conf
       echo "wpa_key_mgmt=WPA-PSK" >> /etc/hostapd/hostapd.conf
       echo "wpa_pairwise=TKIP" >> /etc/hostapd/hostapd.conf
       echo "rsn_pairwise=CCMP" >> /etc/hostapd/hostapd.conf
+      echo "ignore_broadcast_ssid=0" >> /etc/hostapd/hostapd.conf
+      echo "eap_reauth_period=360000000" >> /etc/hostapd/hostapd.conf
+      echo "ssid=RouterX86" >> /etc/hostapd/hostapd.conf
       echo "wpa_passphrase=RouterX86" >> /etc/hostapd/hostapd.conf
+      echo "" >> /etc/hostapd/hostapd.conf
+      echo "# Tarjeta Compex WLE1216v5" >> /etc/hostapd/hostapd.conf
+      echo "driver=nl80211" >> /etc/hostapd/hostapd.conf
+      echo "channel=0     # El canal a usar. 0 significa que buscará automáticamente el canal con menos interferencias" >> /etc/hostapd/hostapd.conf
+      echo "hw_mode=a" >> /etc/hostapd/hostapd.conf
+      echo "ieee80211n=1" >> /etc/hostapd/hostapd.conf
+      echo "ieee80211ac=1" >> /etc/hostapd/hostapd.conf
+      echo "wme_enabled=1" >> /etc/hostapd/hostapd.conf
+      echo "wmm_enabled=1 # Soporte para QoS" >> /etc/hostapd/hostapd.conf
+      echo "ieee80211d=1  # Limitar las frecuencias sólo a las disponibles en el país" >> /etc/hostapd/hostapd.conf
+      echo "country_code=ES" >> /etc/hostapd/hostapd.conf
+      echo "ht_capab=[RXLDPC][HT20+][HT40+][SHORT-GI-20][SHORT-GI-40][TX-STBC][RX-STBC1][MAX-AMSDU-7935][DSSS_CCK-40]" >> /etc/hostapd/hostapd.conf
+      echo "vht_capab=[MAX-MPDU-11454][VHT160-80PLUS80][RXLDPC][SHORT-GI-80][SHORT-GI-160][TX-STBC][SU-BEAMFORMER][SU-BEAMFORMEE][MU-BEAMFORMER][MU-BEAMFORMEE][RX-ANTENNA-PATTERN][TX-ANTENNA-PATTERN]" >> /etc/hostapd/hostapd.conf
+      echo ""
+      
       echo ""
       echo "----------------------------------"
       echo "  CONFIGURANDO INTERFACES DE RED"
