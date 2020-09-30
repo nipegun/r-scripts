9f16ab4f47cf550c73babe336b75d12556897028
NiPeGun <nipegun@gmail.com>
2020-10-01 00:54:46 +0200

    Update InspecccionarSubred.sh

--- InspecccionarSubred.sh
+++ InspecccionarSubred.sh
@@ -31,12 +31,73 @@ if [ $# -ne $CantArgsCorrectos ]
     echo ""
     exit $ArgsInsuficientes
   else
+    apt-get -y update > /dev/null
+    apt-get -y install dialog nmap > /dev/null
+
     echo ""
     echo -e "${ColorVerde}Inspeccionando la subred...${FinColor}"
     echo ""
 
-    apt-get update
-    apt-get -y install nmap > /dev/null
-    nmap -sP $1
+    menu=(dialog --timeout 5 --checklist "Inspeccionar la subred $1:" 22 76 16)
+      opciones=(1 "Inspeccionar sólo enviando ping" off
+                2 "Inspeccionar enviando TCP 3 way handshake" off
+                3 "Inspeccionar enviando TCP pero abortando al recibir el ACK" off
+                4 "" off)
+      choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
+    clear
+
+    for choice in $choices
+      do
+        case $choice in
+
+          1)
+            echo ""
+            echo "Enviando ping a toda la subred..."
+            echo ""
+
+            nmap -sP $1 > /tmp/SubredPing.txt
+
+            echo ""
+            echo "Lista de clientes que respondieron a la petición de ping:"
+            echo ""
+
+            # Limpiar el archivo /tmp/SubredPing.txt quitando lo que no interesa
+            sed -i -e 's|Nmap scan report for ||g' /tmp/SubredPing.txt
+            sed -i -e 's|MAC Address: ||g' /tmp/SubredPing.txt
+            cat /tmp/SubredPing.txt | grep -v "Starting" | grep -v "Host is up" | grep -v "Nmap done" > /tmp/SubredPing.txt
+            # Agregar un espacio tabulado al final de cada línea
+            sed -i 's/$/ ooo\tooo &/' /tmp/SubredPing.txt
+            # Cortar las líneas pares y agregarlas al final de las impares.
+            sed -i 'N;s/\n/ /' /tmp/SubredPing.txt
+            sed -i -e 's|ooo||g' /tmp/SubredPing.txt
+            cat /tmp/SubredPing.txt
+          ;;
+
+          2)
+            echo ""
+            echo "Enviando TCP (SYN---SYN-ACK---ACK---RST-ACK)..."
+            echo ""
+
+            nmap -sT $1 > /tmp/SubredTCP.txt
+            cat /tmp/SubredTCP.txt
+          ;;
+
+          3)
+            echo ""
+            echo "Enviando TCP (SYN---SYN-ACK---RST)..."
+            echo ""
+
+            nmap -sS $1 > /tmp/SubredTCPAborted.txt
+            cat /tmp/SubredTCPAborted.txt
+          ;;
+
+          4)
+
+          ;;
+
+        esac
+
+  done
+
 fi
 
