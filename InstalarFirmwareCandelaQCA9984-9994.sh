4702abf088194cb75d283a1da93e4d2ca4013e39
NiPeGun <nipegun@gmail.com>
2021-02-06 19:27:31 +0100

    Update InstalarFirmwareCandelaQCA9984-9994.sh

--- InstalarFirmwareCandelaQCA9984-9994.sh
+++ InstalarFirmwareCandelaQCA9984-9994.sh
@@ -12,11 +12,19 @@
 ColorVerde="\033[1;32m"
 FinColor="\033[0m"
 
+echo ""
+echo -e "${ColorVerde}--------------------------------------------------------------------------${FinColor}"
+echo -e "${ColorVerde}Iniciando el script de instalación de los drivers ath10k de CandelaTech...${FinColor}"
+echo -e "${ColorVerde}--------------------------------------------------------------------------${FinColor}"
+echo ""
+
 URLFirmAthHTTOpenWrt=https://downloads.openwrt.org/releases/packages-19.07/x86_64/base/ath10k-firmware-qca9984-ct-htt_2020-07-02-1_x86_64.ipk
 
 echo ""
 echo -e "${ColorVerde}Instalando paquetes desde los repositorios...${FinColor}"
 echo ""
+apt-get -y update
+apt-get -y install tar
 apt-get -y purge firmware-atheros
 apt-get -y autoremove
 apt-get -y install firmware-atheros
@@ -29,11 +37,19 @@ cd /tmp
 tar zxpvf /tmp/qca9984-ct-htt.ipk
 tar zxpvf /tmp/data.tar.gz 
 
+echo ""
+echo -e "${ColorVerde}Copiando archivos de firmware a la ubicación correspondiente...${FinColor}"
+echo ""
 # QCA9984 /lib/
 cp /tmp/lib/firmware/ath10k/QCA9984/hw1.0/board-2.bin        -O /lib/firmware/ath10k/QCA9984/hw1.0/board-2.bin
 cp /tmp/lib/firmware/ath10k/QCA9984/hw1.0/ct-firmware-5.bin  -O /lib/firmware/ath10k/QCA9984/hw1.0/firmware-5.bin
-
 # QCA9984 /usr/lib/
 cp /tmp/lib/firmware/ath10k/QCA9984/hw1.0/board-2.bin        -O /usr/lib/firmware/ath10k/QCA9984/hw1.0/board-2.bin
 cp /tmp/lib/firmware/ath10k/QCA9984/hw1.0/ct-firmware-5.bin  -O /usr/lib/firmware/ath10k/QCA9984/hw1.0/firmware-5.bin
+
+echo ""
+echo -e "${ColorVerde}-----------------------------------------------------------------------${FinColor}"
+echo -e "${ColorVerde}Script de instalación de los drivers ath10k de CandelaTech, finalizado.${FinColor}"
+echo -e "${ColorVerde}-----------------------------------------------------------------------${FinColor}"
+echo ""
 
