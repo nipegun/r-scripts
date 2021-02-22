23842ed50666c6241336980cc983f8ad4289f7c0
NiPeGun <nipegun@gmail.com>
2021-02-22 01:24:16 +0100

    Update RoutearPor-eth1-MovistarES-(NanoPi-R2S).sh

--- PostInst/debian10/RoutearPor-eth1-MovistarES-(NanoPi-R2S).sh
+++ PostInst/debian10/RoutearPor-eth1-MovistarES-(NanoPi-R2S).sh
@@ -141,6 +141,10 @@ echo "ip route flush table main"                                                
 echo "ip route add default dev ppp0 scope link"                                                   >> /root/scripts/ConfigurarRutas.sh
 echo "ip route add 192.168.0.0/24 dev $InterfazLAN proto kernel scope link src "'$'"IPLAN"        >> /root/scripts/ConfigurarRutas.sh
 echo "ip route add 192.168.144.1 dev ppp0 proto kernel scope link src "'$'"IPMovistarWAN"         >> /root/scripts/ConfigurarRutas.sh
+echo "ip route add 192.168.1.0/24 via 192.168.0.2"                                                >> /root/scripts/ConfigurarRutas.sh
+echo 'echo ""'                                                                                    >> /root/scripts/ConfigurarRutas.sh
+echo "ip route"                                                                                   >> /root/scripts/ConfigurarRutas.sh
+echo 'echo ""'                                                                                    >> /root/scripts/ConfigurarRutas.sh
 echo "#/root/scripts/ConfigurarRutas.sh" >> /root/scripts/ComandosPostArranque.sh
 
 echo ""
