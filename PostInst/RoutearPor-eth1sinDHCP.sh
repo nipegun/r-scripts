8f5bc1b6278e9c227eca01370953d2952c22e293
NiPeGun <nipegun@gmail.com>
2022-06-24 03:23:03 +0200

    Update RoutearPor-eth1sinDHCP.sh

--- PostInst/RoutearPor-eth1sinDHCP.sh
+++ PostInst/RoutearPor-eth1sinDHCP.sh
@@ -175,20 +175,20 @@ elif [ $OS_VERS == "11" ]; then
   echo ""
   echo "  Configurando la interfaz loopback"
   echo ""
-  echo "auto lo"                                                    > /etc/network/interfaces
-  echo "  iface lo inet loopback"                                  >> /etc/network/interfaces
-  echo "  pre-up iptables-restore < /root/ReglasIPTablesIP4Router" >> /etc/network/interfaces
-  echo ""                                                          >> /etc/network/interfaces
+  echo "auto lo"                                 > /etc/network/interfaces
+  echo "  iface lo inet loopback"               >> /etc/network/interfaces
+  echo "  pre-up nft --file /etc/nftables.conf" >> /etc/network/interfaces
+  echo ""                                       >> /etc/network/interfaces
 
   echo ""
   echo "  Configurando la 1ra interfaz ethernet"
   echo ""
-  echo "auto $interfazcableada1"              >> /etc/network/interfaces
-  echo "  allow-hotplug $interfazcableada1"   >> /etc/network/interfaces
-  echo "  iface $interfazcableada1 inet dhcp" >> /etc/network/interfaces
-  echo ""                                     >> /etc/network/interfaces
-
+  echo "auto $interfazcableada1"                >> /etc/network/interfaces
+  echo "  allow-hotplug $interfazcableada1"     >> /etc/network/interfaces
+  echo "  iface $interfazcableada1 inet dhcp"   >> /etc/network/interfaces
+  echo ""                                       >> /etc/network/interfaces
   echo ""
+
   echo "  Configurando la 2da interfaz ethenet"
   echo ""
   echo "auto $interfazcableada2"                >> /etc/network/interfaces
@@ -200,37 +200,39 @@ elif [ $OS_VERS == "11" ]; then
   echo ""                                       >> /etc/network/interfaces
 
   echo ""
-  echo "  Habilitando el forwarding entre interfaces de red..."
+  echo "  Habilitando el forwarding entre interfaces..."
   echo ""
   cp /etc/sysctl.conf /etc/sysctl.conf.bak
   sed -i -e 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|g' /etc/sysctl.conf
 
-  echo ""
-  echo "  Creando las reglas de IPTables..."
-  echo ""
-  echo "*mangle"                                                                                                > /root/ReglasIPTablesIP4Router
-  echo ":PREROUTING ACCEPT [0:0]"                                                                              >> /root/ReglasIPTablesIP4Router
-  echo ":INPUT ACCEPT [0:0]"                                                                                   >> /root/ReglasIPTablesIP4Router
-  echo ":FORWARD ACCEPT [0:0]"                                                                                 >> /root/ReglasIPTablesIP4Router
-  echo ":OUTPUT ACCEPT [0:0]"                                                                                  >> /root/ReglasIPTablesIP4Router
-  echo ":POSTROUTING ACCEPT [0:0]"                                                                             >> /root/ReglasIPTablesIP4Router
-  echo "COMMIT"                                                                                                >> /root/ReglasIPTablesIP4Router
-  echo ""                                                                                                      >> /root/ReglasIPTablesIP4Router
-  echo "*nat"                                                                                                  >> /root/ReglasIPTablesIP4Router
-  echo ":PREROUTING ACCEPT [0:0]"                                                                              >> /root/ReglasIPTablesIP4Router
-  echo ":INPUT ACCEPT [0:0]"                                                                                   >> /root/ReglasIPTablesIP4Router
-  echo ":OUTPUT ACCEPT [0:0]"                                                                                  >> /root/ReglasIPTablesIP4Router
-  echo ":POSTROUTING ACCEPT [0:0]"                                                                             >> /root/ReglasIPTablesIP4Router
-  echo "-A POSTROUTING -o $interfazcableada1 -j MASQUERADE"                                                    >> /root/ReglasIPTablesIP4Router
-  echo "COMMIT"                                                                                                >> /root/ReglasIPTablesIP4Router
-  echo ""                                                                                                      >> /root/ReglasIPTablesIP4Router
-  echo "*filter"                                                                                               >> /root/ReglasIPTablesIP4Router
-  echo ":INPUT ACCEPT [0:0]"                                                                                   >> /root/ReglasIPTablesIP4Router
-  echo ":FORWARD ACCEPT [0:0]"                                                                                 >> /root/ReglasIPTablesIP4Router
-  echo ":OUTPUT ACCEPT [0:0]"                                                                                  >> /root/ReglasIPTablesIP4Router
-  echo "-A FORWARD -i $interfazcableada1 -o $interfazcableada2 -m state --state RELATED,ESTABLISHED -j ACCEPT" >> /root/ReglasIPTablesIP4Router
-  echo "-A FORWARD -i $interfazcableada2 -o $interfazcableada1 -j ACCEPT"                                      >> /root/ReglasIPTablesIP4Router
-  echo "COMMIT"                                                                                                >> /root/ReglasIPTablesIP4Router
+  # Crear las reglas
+    echo "table inet filter {"                                                   > /root/ReglasNFTablesNAT.rules
+    echo "}"                                                                    >> /root/ReglasNFTablesNAT.rules
+    echo ""                                                                     >> /root/ReglasNFTablesNAT.rules
+    echo "table ip nat {"                                                       >> /root/ReglasNFTablesNAT.rules
+    echo "  chain postrouting {"                                                >> /root/ReglasNFTablesNAT.rules
+    echo "    type nat hook postrouting priority 100; policy accept;"           >> /root/ReglasNFTablesNAT.rules
+    echo '    oifname "eth0" ip saddr 192.168.1.0/24 counter masquerade'        >> /root/ReglasNFTablesNAT.rules
+    echo "  }"                                                                  >> /root/ReglasNFTablesNAT.rules
+    echo ""                                                                     >> /root/ReglasNFTablesNAT.rules
+    echo "  chain prerouting {"                                                 >> /root/ReglasNFTablesNAT.rules
+    echo "    type nat hook prerouting priority 0; policy accept;"              >> /root/ReglasNFTablesNAT.rules
+    echo '    iifname "eth0" tcp dport 33892 counter dnat to "$vSubred".2:3389' >> /root/ReglasNFTablesNAT.rules
+    echo '    iifname "eth0" tcp dport 33893 counter dnat to "$vSubred".3:3389' >> /root/ReglasNFTablesNAT.rules
+    echo '    iifname "eth0" tcp dport 33894 counter dnat to "$vSubred".4:3389' >> /root/ReglasNFTablesNAT.rules
+    echo "  }"                                                                  >> /root/ReglasNFTablesNAT.rules
+    echo "}"                                                                    >> /root/ReglasNFTablesNAT.rules
+
+  # Agregar las reglas al archivo de configuración de NFTables
+    sed -i '/^flush ruleset/a include "/root/ReglasNFTablesNAT.rules"' /etc/nftables.conf
+    sed -i -e 's|flush ruleset|flush ruleset\n|g'                      /etc/nftables.conf
+
+  # Recargar las reglas generales de NFTables
+    nft --file /etc/nftables.conf
+
+  # Agregar las reglas a los ComandosPostArranque
+    sed -i -e 's|nft --file /etc/nftables.conf||g' /root/scripts/ComandosPostArranque.sh
+    echo "nft --file /etc/nftables.conf" >>        /root/scripts/ComandosPostArranque.sh
 
 fi
 
