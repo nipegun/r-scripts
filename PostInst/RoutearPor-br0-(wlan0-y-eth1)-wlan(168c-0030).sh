510ec42d7f6031c206c09a3efa692c89336f1acf
NiPeGun <nipegun@gmail.com>
2022-07-21 06:40:09 +0200

    Update RoutearPor-br0-(wlan0-y-eth1)-wlan(168c-0030).sh

--- PostInst/RoutearPor-br0-(wlan0-y-eth1)-wlan(168c-0030).sh
+++ PostInst/RoutearPor-br0-(wlan0-y-eth1)-wlan(168c-0030).sh
@@ -582,15 +582,15 @@ elif [ $OS_VERS == "11" ]; then
           echo "  Agregando todos los repositorios..."
           echo ""
           cp /etc/apt/sources.list /etc/apt/sources.list.bak
-          echo "deb http://deb.debian.org/debian/ bullseye main contrib non-free"              > /etc/apt/sources.list
-          echo "deb-src http://deb.debian.org/debian/ bullseye main contrib non-free"         >> /etc/apt/sources.list
-          echo ""                                                                             >> /etc/apt/sources.list
-          echo "deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free"     >> /etc/apt/sources.list
-          echo "deb-src http://deb.debian.org/debian/ bullseye-updates main contrib non-free" >> /etc/apt/sources.list
-          echo ""                                                                             >> /etc/apt/sources.list
-          echo "deb http://security.debian.org/ bullseye/updates main contrib non-free"       >> /etc/apt/sources.list
-          echo "deb-src http://security.debian.org/ bullseye/updates main contrib non-free"   >> /etc/apt/sources.list
-          echo ""                                                                             >> /etc/apt/sources.list
+          echo "deb http://deb.debian.org/debian bullseye main contrib non-free"                         > /etc/apt/sources.list
+          echo "deb-src http://deb.debian.org/debian bullseye main contrib non-free"                    >> /etc/apt/sources.list
+          echo ""                                                                                       >> /etc/apt/sources.list
+          echo "deb http://deb.debian.org/debian-security/ bullseye-security main contrib non-free"     >> /etc/apt/sources.list
+          echo "deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free" >> /etc/apt/sources.list
+          echo ""                                                                                       >> /etc/apt/sources.list
+          echo "deb http://deb.debian.org/debian bullseye-updates main contrib non-free"                >> /etc/apt/sources.list
+          echo "deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free"            >> /etc/apt/sources.list
+          echo ""                                                                                       >> /etc/apt/sources.list
         ;;
 
         2)
