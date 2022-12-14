79fd22a0a7c5d813b94f72dceab411e129b65ef8
NiPeGun <nipegun@gmail.com>
2022-12-14 18:15:27 +0100

    Update PuntoDeAcceso-br0-(eth0-y-wlan0)-WiFi(0b05-17d1).sh

--- PostInst/PuntoDeAcceso-br0-(eth0-y-wlan0)-WiFi(0b05-17d1).sh
+++ PostInst/PuntoDeAcceso-br0-(eth0-y-wlan0)-WiFi(0b05-17d1).sh
@@ -48,7 +48,8 @@ echo "  wme_enabled=1"                                                          
 echo "  wmm_enabled=1                           # Soporte para QoS"                                                >> /etc/hostapd/hostapd.conf
 echo "  ieee80211d=1                            # Limitar las frecuencias sólo a las disponibles en el país"       >> /etc/hostapd/hostapd.conf
 echo "  country_code=ES"                                                                                           >> /etc/hostapd/hostapd.conf
-echo "  ht_capab=[SHORT-GI-20][SHORT-GI-40][RX-STBC1][MAX-AMSDU-3839][DSSS_CCK-40]"                                >> /etc/hostapd/hostapd.conf
+echo "  ht_capab=[SHORT-GI-40][RX-STBC1][MAX-AMSDU-3839][DSSS_CCK-40]"                                             >> /etc/hostapd/hostapd.conf
+echo "  #[HT20][SHORT-GI-20] dejados fuera para forzar que la red n se cree en el canal de 40Mhz"                  >> /etc/hostapd/hostapd.conf
 echo "  vht_capab=[MAX-MPDU-3895][VHT160-80PLUS80][SHORT-GI-80][RX-ANTENNA-PATTERN][TX-ANTENNA-PATTERN]"           >> /etc/hostapd/hostapd.conf
 
 echo ""
@@ -78,70 +79,6 @@ echo "  wme_enabled=1"                                                          
 echo "  wmm_enabled=1                           # Soporte para QoS"                                                >> /etc/hostapd/hostapd.conf
 echo "  ieee80211d=1                            # Limitar las frecuencias sólo a las disponibles en el país"       >> /etc/hostapd/hostapd.conf
 echo "  country_code=ES"                                                                                           >> /etc/hostapd/hostapd.conf
-echo "  ht_capab=[SHORT-GI-20][SHORT-GI-40][RX-STBC1][MAX-AMSDU-3839][DSSS_CCK-40]"                                >> /etc/hostapd/hostapd.conf
+echo "  ht_capab=[SHORT-GI-40][RX-STBC1][MAX-AMSDU-3839][DSSS_CCK-40]"                                             >> /etc/hostapd/hostapd.conf
+echo "  #[HT20][SHORT-GI-20] dejados fuera para forzar que la red n se cree en el canal de 40Mhz"                  >> /etc/hostapd/hostapd.conf
 echo "  vht_capab=[MAX-MPDU-3895][VHT160-80PLUS80][SHORT-GI-80][RX-ANTENNA-PATTERN][TX-ANTENNA-PATTERN]"           >> /etc/hostapd/hostapd.conf
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      echo "ht_capab=[RXLDPC][HT40+][SHORT-GI-40][TX-STBC][RX-STBC1][MAX-AMSDU-7935][DSSS_CCK-40]"                                                                                                       >> /etc/hostapd/hostapd.conf
-      echo "#[HT20][SHORT-GI-20] dejados fuera para forzar que la red n se cree en el canal de 40Mhz"                                                                                                    >> /etc/hostapd/hostapd.conf
-      echo "vht_capab=[MAX-MPDU-11454][VHT160-80PLUS80][RXLDPC][SHORT-GI-80][SHORT-GI-160][TX-STBC][SU-BEAMFORMER][SU-BEAMFORMEE][MU-BEAMFORMER][MU-BEAMFORMEE][RX-ANTENNA-PATTERN][TX-ANTENNA-PATTERN]" >> /etc/hostapd/hostapd.conf
-      echo ""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-vInterfazInalambrica="wlan0"
-echo "#/etc/hostapd/hostapd.conf"                                                                > /etc/hostapd/hostapd.conf
-echo "interface=$vInterfazInalambrica"                                                          >> /etc/hostapd/hostapd.conf
-echo "driver=nl80211"                                                                           >> /etc/hostapd/hostapd.conf
-echo "bridge=br0"                                                                               >> /etc/hostapd/hostapd.conf
-echo "hw_mode=g"                                                                                >> /etc/hostapd/hostapd.conf
-echo "wme_enabled=1"                                                                            >> /etc/hostapd/hostapd.conf
-echo "ieee80211n=1"                                                                             >> /etc/hostapd/hostapd.conf
-echo "ieee80211d=1"                                                                             >> /etc/hostapd/hostapd.conf
-echo "channel=1"                                                                                >> /etc/hostapd/hostapd.conf
-echo "country_code=ES"                                                                          >> /etc/hostapd/hostapd.conf
-echo "wmm_enabled=1"                                                                            >> /etc/hostapd/hostapd.conf
-echo "ht_capab=[RX-STBC1][MAX-AMSDU-3839][DSSS_CCK-40]" >> /etc/hostapd/hostapd.conf
-echo "ht_capab=[HT20+][HT40+][SHORT-GI-20][SHORT-GI-40]" >> /etc/hostapd/hostapd.conf
-
-echo "ignore_broadcast_ssid=0"                                                                  >> /etc/hostapd/hostapd.conf
-echo "ssid=RouterX86"                                                                           >> /etc/hostapd/hostapd.conf
-echo "eap_reauth_period=360000000"                                                              >> /etc/hostapd/hostapd.conf
-echo "wpa=2"                                                                                    >> /etc/hostapd/hostapd.conf
-echo "wpa_key_mgmt=WPA-PSK"                                                                     >> /etc/hostapd/hostapd.conf
-echo "wpa_pairwise=TKIP"                                                                        >> /etc/hostapd/hostapd.conf
-echo "rsn_pairwise=CCMP"                                                                        >> /etc/hostapd/hostapd.conf
-echo "wpa_passphrase=RouterX86"                                                                 >> /etc/hostapd/hostapd.conf
-
-
-
