40248cbf68b5f3e598083e1cb86f6c63f3c0c279
NiPeGun <nipegun@gmail.com>
2022-12-15 00:13:26 +0100

    Update PuntoDeAcceso-br0-(eth0-y-wlan0)-WiFi(0cf3-9271).sh

--- PostInst/PuntoDeAcceso-br0-(eth0-y-wlan0)-WiFi(0cf3-9271).sh
+++ PostInst/PuntoDeAcceso-br0-(eth0-y-wlan0)-WiFi(0cf3-9271).sh
@@ -54,7 +54,11 @@ vMacEth0=$(cat /sys/class/net/eth0/address)
   echo ""                           >> /etc/network/interfaces
   echo "#auto br0"                  >> /etc/network/interfaces
   echo "  #iface br0 inet static"   >> /etc/network/interfaces
-  echo "  #bridge-ports eth0 wlan0" >> /etc/network/interfaces
+  echo "  #bridge_ports eth0 wlan0" >> /etc/network/interfaces
+  echo "  #bridge_stp off"          >> /etc/network/interfaces
+  echo "  #bridge_fd 0"             >> /etc/network/interfaces
+  echo "  #bridge_maxwait 0"        >> /etc/network/interfaces
+  echo "  #hwaddress $vMacEth0"     >> /etc/network/interfaces
   echo "  #address 192.168.1.2"     >> /etc/network/interfaces
   echo "  #netmask 255.255.255.0"   >> /etc/network/interfaces
   echo "  #gateway 192.168.1.1"     >> /etc/network/interfaces
