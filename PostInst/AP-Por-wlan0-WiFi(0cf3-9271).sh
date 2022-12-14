29e94399bece9d20da782ab6a0510b36829d483e
NiPeGun <nipegun@gmail.com>
2022-12-14 13:10:52 +0100

    Update AP-Por-wlan0-WiFi(0cf3-9271).sh

--- PostInst/AP-Por-wlan0-WiFi(0cf3-9271).sh
+++ PostInst/AP-Por-wlan0-WiFi(0cf3-9271).sh
@@ -16,6 +16,11 @@ apt-get -y update
 apt-get -y install firmware-ath9k-htc
 apt-get -y install hostapd
 apt-get -y install crda
+
 cp /etc/sysctl.conf /etc/sysctl.conf.bak
 sed -i -e 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|g' /etc/sysctl.conf
+
+cp /etc/default/hostapd /etc/default/hostapd.bak
+sed -i -e 's|#DAEMON_CONF=""|DAEMON_CONF="/etc/hostapd/hostapd.conf"|g' /etc/default/hostapd
+sed -i -e 's|#DAEMON_OPTS=""|DAEMON_OPTS="-dd -t -f /var/log/hostapd.log"|g' /etc/default/hostapd
 
