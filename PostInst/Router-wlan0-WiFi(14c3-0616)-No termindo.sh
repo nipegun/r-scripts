c5a70068e957c74e5c8c1e759a6bee8b7bf5f9b8
NiPeGun <nipegun@gmail.com>
2023-07-03 05:25:28 +0200

    Create Router-wlan0-WiFi(14c3-0616)-No terminado.sh

---
+++ PostInst/Router-wlan0-WiFi(14c3-0616)-No termindo.sh
@@ -0,0 +1,125 @@
+OpenWrt-ax-2-20
+OpenWrt-ax-2-40
+OpenWrt-ax-5-20
+OpenWrt-ax-5-40
+OpenWrt-ax-5-80
+OpenWrt-ax-5-160
+
+Wiphy phy0
+  Supported interface modes:
+    * managed
+    * AP
+    * AP/VLAN
+    * monitor
+  Band 1:
+    Capabilities: 0x9ff
+      RX LDPC
+      HT20/HT40
+      SM Power Save disabled
+      RX Greenfield
+      RX HT20 SGI
+      RX HT40 SGI
+      TX STBC
+      RX STBC 1-stream
+      Max AMSDU length: 7935 bytes
+      No DSSS/CCK HT40
+    HE Iftypes: AP
+      HE MAC Capabilities (0x00011a081044):
+        +HTC HE Supported
+				BSR
+				OM Control
+				Maximum A-MPDU Length Exponent: 3
+				BQR
+				A-MSDU in A-MPDU
+				OM Control UL MU Data Disable RX
+			HE PHY Capabilities: (0x0220ce120000a000000c00):
+				HE40/2.4GHz
+				LDPC Coding in Payload
+				NDP with 4x HE-LTF and 3.2us GI
+				STBC Tx <= 80MHz
+				STBC Rx <= 80MHz
+				Full Bandwidth UL MU-MIMO
+				Partial Bandwidth UL MU-MIMO
+				DCM Max Constellation: 2
+				DCM Max Constellation Rx: 2
+				Partial Bandwidth Extended Range
+				PPE Threshold Present
+				TX 1024-QAM
+				RX 1024-QAM
+	Band 2:
+		Capabilities: 0x9ff
+			RX LDPC
+			HT20/HT40
+			SM Power Save disabled
+			RX Greenfield
+			RX HT20 SGI
+			RX HT40 SGI
+			TX STBC
+			RX STBC 1-stream
+			Max AMSDU length: 7935 bytes
+			No DSSS/CCK HT40
+		VHT Capabilities (0x339071f6):
+			Max MPDU length: 11454
+			Supported Channel Width: 160 MHz
+			RX LDPC
+			short GI (80 MHz)
+			short GI (160/80+80 MHz)
+			TX STBC
+			SU Beamformee
+			MU Beamformee
+			RX antenna pattern consistency
+			TX antenna pattern consistency
+		HE Iftypes: AP
+			HE MAC Capabilities (0x00011a081044):
+				+HTC HE Supported
+				BSR
+				OM Control
+				Maximum A-MPDU Length Exponent: 3
+				BQR
+				A-MSDU in A-MPDU
+				OM Control UL MU Data Disable RX
+			HE PHY Capabilities: (0x0420ce120000a000000c00):
+				HE40/HE80/5GHz
+				LDPC Coding in Payload
+				NDP with 4x HE-LTF and 3.2us GI
+				STBC Tx <= 80MHz
+				STBC Rx <= 80MHz
+				Full Bandwidth UL MU-MIMO
+				Partial Bandwidth UL MU-MIMO
+				DCM Max Constellation: 2
+				DCM Max Constellation Rx: 2
+				Partial Bandwidth Extended Range
+				PPE Threshold Present
+				TX 1024-QAM
+				RX 1024-QAM
+	Band 4:
+		HE Iftypes: AP
+			HE MAC Capabilities (0x00011a081044):
+				+HTC HE Supported
+				BSR
+				OM Control
+				Maximum A-MPDU Length Exponent: 3
+				BQR
+				A-MSDU in A-MPDU
+				OM Control UL MU Data Disable RX
+			HE PHY Capabilities: (0x0420ce120000a000000c00):
+				HE40/HE80/5GHz
+				LDPC Coding in Payload
+				NDP with 4x HE-LTF and 3.2us GI
+				STBC Tx <= 80MHz
+				STBC Rx <= 80MHz
+				Full Bandwidth UL MU-MIMO
+				Partial Bandwidth UL MU-MIMO
+				DCM Max Constellation: 2
+				DCM Max Constellation Rx: 2
+				Partial Bandwidth Extended Range
+				PPE Threshold Present
+				TX 1024-QAM
+				RX 1024-QAM
+		EHT Iftypes: AP
+			EHT MAC Capabilities (0x0000):
+			EHT PHY Capabilities: (0x0000000000000000):
+			EHT MCS/NSS: (0x):
+			EHT bw <= 80 MHz, max NSS for MCS 8-9: Rx=0, Tx=0
+			EHT bw <= 80 MHz, max NSS for MCS 10-11: Rx=0, Tx=0
+			EHT bw <= 80 MHz, max NSS for MCS 12-13: Rx=0, Tx=0
