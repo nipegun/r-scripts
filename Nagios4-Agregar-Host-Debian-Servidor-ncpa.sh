313f9fbf0d0f3d90a2a56c05dcf4417e710b13db
NiPeGun <nipegun@gmail.com>
2024-06-14 12:06:41 +0200

    Update Nagios4-Agregar-Host-Debian-Servidor-ncpa.sh

--- Nagios4-Agregar-Host-Debian-Servidor-ncpa.sh
+++ Nagios4-Agregar-Host-Debian-Servidor-ncpa.sh
@@ -7,10 +7,10 @@
 # Script de NiPeGun para agregar un servidor Debian a Nagios
 #
 # Ejecución remota:
-#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/Nagios4-Agregar-Host-Debian-Servidor.sh | bash -s URL Servicio
+#  curl -sL https://raw.githubusercontent.com/nipegun/r-scripts/master/Nagios4-Agregar-Host-Debian-Servidor.sh | bash -s URL Servicio
 #
 #  Ejemplo:
-#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/Nagios4-Agregar-Host-Debian-Servidor.sh | bash -s midebianserver "Mi servidor Debian" "192.168.0.123"
+#  curl -sL https://raw.githubusercontent.com/nipegun/r-scripts/master/Nagios4-Agregar-Host-Debian-Servidor.sh | bash -s midebianserver "Mi servidor Debian" "192.168.0.123"
 # ----------
 
 # Definir constantes de color
