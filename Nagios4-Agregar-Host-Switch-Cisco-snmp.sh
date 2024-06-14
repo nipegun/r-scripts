8c06dcfe59b7afb8cb1d68963195b1d48d0f96bf
NiPeGun <nipegun@gmail.com>
2024-06-14 12:06:58 +0200

    Update Nagios4-Agregar-Host-Switch-Cisco-snmp.sh

--- Nagios4-Agregar-Host-Switch-Cisco-snmp.sh
+++ Nagios4-Agregar-Host-Switch-Cisco-snmp.sh
@@ -9,10 +9,10 @@
 # Script de NiPeGun para agregar un host Switch de Cisco a Nagios
 #
 # Ejecución remota:
-#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/Nagios4-Agregar-Host-Switch-Cisco-snmp.sh | bash -s URL Servicio
+#  curl -sL https://raw.githubusercontent.com/nipegun/r-scripts/master/Nagios4-Agregar-Host-Switch-Cisco-snmp.sh | bash -s URL Servicio
 #
 #  Ejemplo:
-#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/Nagios4-Agregar-Host-Switch-Cisco-snmp.sh | bash -s miswitch "Mi switch" "192.168.0.123"
+#  curl -sL https://raw.githubusercontent.com/nipegun/r-scripts/master/Nagios4-Agregar-Host-Switch-Cisco-snmp.sh | bash -s miswitch "Mi switch" "192.168.0.123"
 # ----------
 
 # Definir constantes de color
