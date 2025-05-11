f3aa0572b4a0234dc5be78bd1968cb23bd972f89
NiPeGun <nipegun@gmail.com>
2025-05-11 20:13:45 +0200

    Update hostapd-Instalar.sh

--- SoftInst/ParaCLI/hostapd-Instalar.sh
+++ SoftInst/ParaCLI/hostapd-Instalar.sh
@@ -1,10 +1,16 @@
 
-apt-get -y autoremove hostapd
+sudo apt-get -y autoremove hostapd
+
+sudo apt-get -y install build-essential
+sudo apt-get -y install pkg-config
+sudo apt-get -y install libnl-3-dev
+ 
 
 cd /tmp
+sudo rm -rf /tmp/hostapd
 git clone https://w1.fi/hostap.git
 cd hostap/hostapd
 cp defconfig .config
-sudo apt-get -y install pkg-config
+
 make -j$(nproc)
 sudo cp hostapd /usr/local/bin/
