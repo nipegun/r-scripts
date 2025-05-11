37335d78a6142d3b9c2d77d4e714733497a7894a
NiPeGun <nipegun@gmail.com>
2025-05-11 20:37:02 +0200

    Update hostapd-Instalar.sh

--- SoftInst/ParaCLI/hostapd-Instalar.sh
+++ SoftInst/ParaCLI/hostapd-Instalar.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-# Instalar herramientas para compilar
-  sudo apt-get -y install build-essential
-  sudo apt-get -y install pkg-config
-  sudo apt-get -y install libnl-3-dev
-  sudo apt-get -y install libssl-dev
-  sudo apt-get -y install libnl-genl-3-dev
-
-# Clonar repositorio con la última versión
-  cd /tmp
-  sudo rm -rf /tmp/hostap
-  git clone https://w1.fi/hostap.git
-
-# Compilar
-  cd hostap/hostapd
-  cp defconfig .config
-  make -j$(nproc)
+# Desinstalar todo lo instalado previamente
+  mv /etc/hostapd/hostapd.conf /tmp/
+  apt-get -y autoremove --purge hostapd
+  rm -rf /etc/hostapd/
 
 # Instalar el paquete oficial del repo de Dbian
   sudo apt-get -y install hostapd
@@ -36,5 +24,24 @@
   sudo systemctl enable hostapd --now
   sudo systemctl status hostapd --no-pager
 
+
+# Instalar herramientas para compilar
+  sudo apt-get -y install build-essential
+  sudo apt-get -y install pkg-config
+  sudo apt-get -y install libnl-3-dev
+  sudo apt-get -y install libssl-dev
+  sudo apt-get -y install libnl-genl-3-dev
+
+# Clonar repositorio con la última versión
+  cd /tmp
+  sudo rm -rf /tmp/hostap
+  git clone https://w1.fi/hostap.git
+
+# Compilar
+  cd hostap/hostapd
+  cp defconfig .config
+  make -j$(nproc)
+
 # Copiar el binario compilado reemplazando el binario instalado desde el repo
   sudo cp -fv hostapd /usr/sbin/
+
