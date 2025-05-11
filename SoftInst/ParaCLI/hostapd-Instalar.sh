e24baaf452abbd7e62c1246224ca9125e382110a
NiPeGun <nipegun@gmail.com>
2025-05-11 20:15:27 +0200

    Update hostapd-Instalar.sh

--- SoftInst/ParaCLI/hostapd-Instalar.sh
+++ SoftInst/ParaCLI/hostapd-Instalar.sh
@@ -5,6 +5,7 @@ sudo apt-get -y install build-essential
 sudo apt-get -y install pkg-config
 sudo apt-get -y install libnl-3-dev
 sudo apt-get -y install libssl-dev
+sudo apt-get -y install libnl-genl-3-dev
 
 cd /tmp
 sudo rm -rf /tmp/hostapd
