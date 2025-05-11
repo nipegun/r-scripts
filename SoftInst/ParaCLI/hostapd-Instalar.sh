676e9f845b2a801f7fbf4c778d0b53d5b27041a1
NiPeGun <nipegun@gmail.com>
2025-05-11 20:28:11 +0200

    Update hostapd-Instalar.sh

--- SoftInst/ParaCLI/hostapd-Instalar.sh
+++ SoftInst/ParaCLI/hostapd-Instalar.sh
@@ -1,16 +1,31 @@
+#!/bin/bash
 
-sudo apt-get -y install hostapd
-sudo apt-get -y install build-essential
-sudo apt-get -y install pkg-config
-sudo apt-get -y install libnl-3-dev
-sudo apt-get -y install libssl-dev
-sudo apt-get -y install libnl-genl-3-dev
+# Instalar herramientas para compilar
+  sudo apt-get -y install build-essential
+  sudo apt-get -y install pkg-config
+  sudo apt-get -y install libnl-3-dev
+  sudo apt-get -y install libssl-dev
+  sudo apt-get -y install libnl-genl-3-dev
 
-cd /tmp
-sudo rm -rf /tmp/hostap
-git clone https://w1.fi/hostap.git
-cd hostap/hostapd
-cp defconfig .config
+# Clonar repositorio con la última versión
+  cd /tmp
+  sudo rm -rf /tmp/hostap
+  git clone https://w1.fi/hostap.git
 
-make -j$(nproc)
-sudo cp -fv hostapd /usr/sbin/
+# Compilar
+  cd hostap/hostapd
+  cp defconfig .config
+  make -j$(nproc)
+
+# Instalar el paquete oficial del repo de Dbian
+  sudo apt-get -y install hostapd
+
+# Desenmascarar y activar el servicio (Si no, con entorno gráfico NetworkManager no lo deja iniciar)
+  echo ""
+  echo "  Desenmascarando y activando el servicio hostapd..."
+  echo ""
+  sudo systemctl unmask hostapd
+  sudo systemctl enable hostapd --now
+
+# Copiar el binario compilado reemplazando el binario instalado desde el repo
+  sudo cp -fv hostapd /usr/sbin/
