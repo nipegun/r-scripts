4077a930924e1b2297df22ba5bc431b01abfb370
NiPeGun <nipegun@gmail.com>
2025-05-11 20:16:44 +0200

    Update hostapd-Instalar.sh

--- SoftInst/ParaCLI/hostapd-Instalar.sh
+++ SoftInst/ParaCLI/hostapd-Instalar.sh
@@ -8,7 +8,7 @@ sudo apt-get -y install libssl-dev
 sudo apt-get -y install libnl-genl-3-dev
 
 cd /tmp
-sudo rm -rf /tmp/hostapd
+sudo rm -rf /tmp/hostap
 git clone https://w1.fi/hostap.git
 cd hostap/hostapd
 cp defconfig .config
